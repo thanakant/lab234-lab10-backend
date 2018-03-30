ec695bc82881dcfface68375808f24bbe5e820c0
Unknown <interstinger@gmail.com>
2018-03-30 16:25:39 +0700

    Change class name

--- src/test/java/camt/se234/lab10/service/TestGradeService.java
+++ src/test/java/camt/se234/lab10/service/TestGradeService.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-public  class GradeServiceImplTest {
+public  class TestGradeService {
     @Test
     public void testGetGrade(){
         GradeServiceImpl gradeService = new GradeServiceImpl();
